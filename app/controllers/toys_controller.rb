c3dde4f36819b127aa3dc3b83d216a5868f0819f
poklipart <angellopezandrade4@gmail.com>
2019-10-14 16:00:10 +1100

    completed core

---
+++ app/controllers/toys_controller.rb
@@ -0,0 +1,3 @@
+class Toys < ApplicationController
+    
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+for i in 1..20
+    Toy.create(
+        name: Faker::Hipster.word.capitalize,
+        description: "The back of the box says: #{Faker::TvShows::RickAndMorty.quote}.",
+        date: "#{rand(1400..2019)}/#{rand(1..12)}/#{rand(1..31)}",
+        user: Faker::TvShows::RickAndMorty.character
+    )
+    puts "Created #{i} toys."
+end
