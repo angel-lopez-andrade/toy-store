c46f8b7a9dddabc0aae9bd818d3e1c5a4aa30c38
poklipart <angellopezandrade4@gmail.com>
2019-10-14 17:04:25 +1100

    comprehensive progression

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,3 +1,26 @@
-class Toys < ApplicationController
-    
+class ToysController < ApplicationController
+    def index
+        @toys = Toy.all
+    end
+
+    def create
+    end
+
+    def new
+        @toys = Toy.all
+    end
+
+    def edit
+        @toys = Toy.all
+    end
+
+    def show
+        @toys = Toy.all
+    end
+
+    def update
+    end
+
+    def destroy
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/toys", to: "toys#index", as: "toys"
+  post "/toys", to: "toys#create"
+  get "/toys/new", to: "toys#new", as: "new_toy"
+  get "/toys/:id/edit", to: "toys#edit", as: "edit_toy"
+  get "/toys/:id", to: "toys#show", as: "toy"
+  put "/toys/:id", to: "toys#update"
+  patch "/toys/:id", to: "toys#update"
+  delete "/toys/:id", to: "toys#destroy"
 end
